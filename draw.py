6964dbd4f8f65c5c16d0c75e8535b7b5eccea263
Stephan Doan <sdoan@stuy.edu>
2017-02-14 11:49:36 -0500

    starting design

--- draw.py
+++ draw.py
@@ -64,3 +64,67 @@ def draw_line7( screen, x0, y0, x1, y1, color ):
         y-=1
         d-=b
  
+def draw_line4( screen, x0, y0, x1, y1, color ):
+    b = x0-x1
+    a = y1-y0
+    d = 2*a + b
+    x = x0
+    y = y0
+    a *= 2
+    b *= 2
+    while x>=x1:
+        plot(screen, color, x, y)
+        if d<0:
+            y+=1
+            d+=b
+        x-=1
+        d-=a
+
+def draw_line3( screen, x0, y0, x1, y1, color ):
+    b = x0-x1
+    a = y1-y0
+    d = a + 2*b
+    x = x0
+    y = y0
+    a *= 2
+    b *= 2
+    while y<=y1:
+        plot(screen, color, x, y)
+        if d>0:
+            x-=1
+            d-=a
+        y+=1
+        d+=b
+ 
+def draw_line5( screen, x0, y0, x1, y1, color ):
+    b = x0-x1
+    a = y1-y0
+    d = 2*a + b
+    x = x0
+    y = y0
+    a *= 2
+    b *= 2
+    while x>=x1:
+        plot(screen, color, x, y)
+        if d>0:
+            y-=1
+            d-=b
+        x-=1
+        d-=a
+
+def draw_line6( screen, x0, y0, x1, y1, color ):
+    b = x0-x1
+    a = y1-y0
+    d = a + 2*b
+    x = x0
+    y = y0
+    a *= 2
+    b *= 2
+    while y>=y1:
+        plot(screen, color, x, y)
+        if d<0:
+            x-=1
+            d-=a
+        y-=1
+        d-=b
+ 
--- main.py
+++ main.py
@@ -4,12 +4,24 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
+for i in range(250):
+    color = [i, i, i]
+    draw_line1(screen, 0, 0, 500, 500-i, color)
+    draw_line3(screen, 500, 0, 250-i, 500, color)
+    draw_line5(screen, 500, 500, 0, 250+i, color)
+    draw_line7(screen, 0, 500, 500-i, 0, color)
 for i in range(200):
-    color = [0, 255-i, i]
+    color = [i/2, 0, 200-i/2]
+    draw_line2(screen, 250+i, 50, 450, 450, color)
+    draw_line4(screen, 450, 250+i, 50, 450, color)
+    draw_line6(screen, 250-i, 450, 50, 50, color)
+    draw_line8(screen, 50, 250-i, 450, 50, color)
+for i in range(200):
+    color = [0, 255-i, 200-i]
     draw_line1(screen, 0, 0, 500, 200-i, color)
-    draw_line2(screen, 0, 0, 200-i, 500, color)
-    draw_line8(screen, 0, 500, 500, 200-i, color)
+    draw_line3(screen, 500, 0, 300+i, 500, color)
+    draw_line5(screen, 500, 500, 0, 300+i, color)
     draw_line7(screen, 0, 500, 200-i, 0, color)
-  
+    
 display(screen)
 save_extension(screen, 'img.png')
