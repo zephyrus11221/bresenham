6de5354b1114f41ae26e0bb8b62e53b6e00008c9
Stephan Doan <sdoan@stuy.edu>
2017-02-16 00:40:00 -0500

    reset code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
-'''
+
 for i in range(251):
     color = [i/2, i/2, i]
     draw_line1(screen, 0, 0, 500, 400-i, color)
@@ -22,10 +22,12 @@ for i in range(201):
     draw_line3(screen, 500, 0, 300+i, 500, color)
     draw_line5(screen, 500, 500, 0, 300+i, color)
     draw_line7(screen, 0, 500, 200-i, 0, color)
-'''
 
+'''
+not working
 for i in range(251):
     draw_line_less(screen, 0, 250, 500, 375-i, color)
+'''
 display(screen)
 save_extension(screen, 'img.png')
 print 'look at img.png'
